2f4082cde30b5b53d2fee3ce8af298d918d254e2
Chris Metz <cm@3d-zeitschrift.de>
2016-05-09 13:40:34 +0200

    having the native script template files organised in the same folder structure
    NOTE: this code is only meant to show the idea, currently native script fails to find the template files on runtime for some reason.

--- src/client/app/frameworks/core.framework/services/view-broker.service.ts
+++ src/client/app/frameworks/core.framework/services/view-broker.service.ts
@@ -3,12 +3,7 @@ import {CoreConfigService} from './core-config.service';
 export class ViewBrokerService {
   
   public static TEMPLATE_URL(path: string): string {  
-    if (CoreConfigService.IS_MOBILE_NATIVE()) {
-      // views for native should all come from ./frameworks/nativescript.framework
-      path = path.slice(1); // remove leading '.'
-      return `./frameworks/nativescript.framework${path}`; 
-    } else {
-      return path;
-    } 
+    // CAUTION: this currently breaks on runtime because nativescript does not find the xml files (although they are there)
+    return path.replace(".html", ".xml"); 
   }
 }
